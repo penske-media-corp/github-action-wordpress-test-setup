da45744c40519928d2aa00fcee91ba763527644c
Erick Hitter <git-contrib@ethitter.com>
2022-08-12 16:22:38 -0700

    Improve SSH setup

--- scripts/set-up-ssh.sh
+++ scripts/set-up-ssh.sh
@@ -8,7 +8,6 @@ fi
 ssh-keyscan bitbucket.org >> "${HOME}/.ssh/known_hosts"
 ssh-keyscan github.com >> "${HOME}/.ssh/known_hosts"
 
-eval `ssh-agent`
-ssh-add - <<<"$( echo "${INPUT_SSH_KEY_ENCODED}" | base64 --decode --ignore-garbage )"
-
-ls -lah "${HOME}/.ssh"
+echo "${INPUT_SSH_KEY_ENCODED}" | base64 --decode --ignore-garbage > "${HOME}/.ssh/id_rsa"
+chmod 400 "${HOME}/.ssh/id_rsa"
+ssh-keygen -lf "${HOME}/.ssh/id_rsa"
