343cd054cdde3c3d456963fa23b370c02e1ddd5b
Erick Hitter <git-contrib@ethitter.com>
2022-08-17 14:23:31 -0700

    Is the issue key order/selection?

--- scripts/set-up-ssh.sh
+++ scripts/set-up-ssh.sh
@@ -12,6 +12,9 @@ ssh-keyscan github.com >> "${HOME}/.ssh/known_hosts"
 
 eval `ssh-agent`
 ssh-add - <<<"$(echo "${BITBUCKET_READ_ONLY_SSH_KEY}")"
-#ssh-add - <<<"$(echo "${GITHUB_READ_ONLY_SSH_KEY}")"
+ssh-add - <<<"$(echo "${GITHUB_READ_ONLY_SSH_KEY}")"
+
+echo $SSH_AUTH_SOCK
+echo $SSH_AGENT_PID
 
 ssh-add -l
