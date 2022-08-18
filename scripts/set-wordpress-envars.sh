45ec1b6b2b0f3d46e656df0a69b06579f408c5b1
Erick Hitter <git-contrib@ethitter.com>
2022-08-17 17:19:26 -0700

    Fix WP install

--- scripts/set-wordpress-envars.sh
+++ scripts/set-wordpress-envars.sh
@@ -6,7 +6,7 @@ echo "TMPDIR=${RUNNER_TEMP}" >> $GITHUB_ENV
 echo "WP_CORE_DIR=${RUNNER_TEMP}/wp/core" >> $GITHUB_ENV
 echo "WP_TESTS_DIR=${RUNNER_TEMP}/wp/tests-lib" >> $GITHUB_ENV
 echo "PMC_PLUGINS_DIR=${RUNNER_TEMP}/wp/core/wp-content/plugins/pmc-plugins" >> $GITHUB_ENV
-echo "PMC_PHPUNIT_BOOTSTRAP=${PMC_PLUGINS_DIR}/pmc-unit-test/bootstrap.php" >> $GITHUB_ENV
+echo "PMC_PHPUNIT_BOOTSTRAP=${RUNNER_TEMP}/wp/core/wp-content/plugins/pmc-plugins/pmc-unit-test/bootstrap.php" >> $GITHUB_ENV
 
 if [[ "branch" == "${GITHUB_REF_TYPE}" && "$(pmc_get_git_default_branch)" == ${GITHUB_REF_NAME} ]]; then
   echo "PMC_PHPUNIT_MATCH_BRANCH=${GITHUB_REF_NAME}" >> $GITHUB_ENV
