2a48c70007bb3c474d1fc2669694a9116d8d8675
Erick Hitter <git-contrib@ethitter.com>
2022-08-19 14:00:23 -0700

    Update branch references

--- scripts/add-shared-utilities.sh
+++ scripts/add-shared-utilities.sh
@@ -4,5 +4,5 @@ set -e
 
 CHECKOUT_DIR="/usr/local/bin/pmc"
 
-git clone -b add/support-default-branch-not-master git@bitbucket.org:penskemediacorp/pmc-docker-common-use-shell-scripts.git "${CHECKOUT_DIR}"
+git clone git@bitbucket.org:penskemediacorp/pmc-docker-common-use-shell-scripts.git "${CHECKOUT_DIR}"
 echo "${CHECKOUT_DIR}/bin" >> $GITHUB_PATH
