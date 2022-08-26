03b0c946f6c0722d55cd61a1ea7014882a71275c
Erick Hitter <git-contrib@ethitter.com>
2022-08-26 12:26:12 -0700

    Enable object caching

--- scripts/set-up-wordpress-dependencies.sh
+++ scripts/set-up-wordpress-dependencies.sh
@@ -14,6 +14,11 @@ mv "${RUNNER_TEMP}/plugin-loader.php" "${WP_CONTENT_TARGET_DIR}/client-mu-plugin
 # Install VIP Go's mu-plugins.
 git_checkout "${WP_CONTENT_TARGET_DIR}/mu-plugins" https://github.com/Automattic/vip-go-mu-plugins-built.git 1
 
+# Install memcached drop-in.
+if [[ ! -f "${WP_CONTENT_TARGET_DIR}/object-cache.php" && -f "${WP_CONTENT_TARGET_DIR}/mu-plugins/drop-ins/object-cache/object-cache-stable.php" ]]; then
+  ln -s "${WP_CONTENT_TARGET_DIR}/mu-plugins/drop-ins/object-cache/object-cache-stable.php" "${WP_CONTENT_TARGET_DIR}/object-cache.php"
+fi
+
 # Install pmc-vip-go-plugins.
 if [[ -f "${WP_CONTENT_TARGET_DIR}/plugins/README.md" ]]; then
   rm -rf "${WP_CONTENT_TARGET_DIR}/plugins"
@@ -27,7 +32,7 @@ if [[ -z "${PMC_IS_PMC_PLUGINS}" ]]; then
   checkout_dependencies .
 else
   rm -rf "${PMC_PLUGINS_DIR}"
-  ln -s "$GITHUB_WORKSPACE" "${PMC_PLUGINS_DIR}"
+  ln -s "${GITHUB_WORKSPACE}" "${PMC_PLUGINS_DIR}"
 fi
 
 maybe_switch_branch_for_testing_theme . "${BITBUCKET_BRANCH}"
