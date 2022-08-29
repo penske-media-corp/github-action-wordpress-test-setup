f42141ec4ccb544278a4d0bc37309edaae55c73c
Erick Hitter <git-contrib@ethitter.com>
2022-08-29 16:33:03 -0700

    Debug

--- scripts/set-wordpress-envars.sh
+++ scripts/set-wordpress-envars.sh
@@ -8,6 +8,7 @@ echo "WP_TESTS_DIR=${RUNNER_TEMP}/wp/tests-lib" >> $GITHUB_ENV
 echo "PMC_PLUGINS_DIR=${RUNNER_TEMP}/wp/core/wp-content/plugins/pmc-plugins" >> $GITHUB_ENV
 echo "PMC_PHPUNIT_BOOTSTRAP=${RUNNER_TEMP}/wp/core/wp-content/plugins/pmc-plugins/pmc-unit-test/bootstrap.php" >> $GITHUB_ENV
 
+echo $(pmc_get_git_default_branch)
 if [[ "branch" == "${GITHUB_REF_TYPE}" && "$(pmc_get_git_default_branch)" == ${GITHUB_REF_NAME} ]]; then
   echo "PMC_PHPUNIT_MATCH_BRANCH=${GITHUB_REF_NAME}" >> $GITHUB_ENV
 else
