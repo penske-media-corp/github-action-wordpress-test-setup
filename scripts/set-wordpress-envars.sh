de0005c335d3e9e9778c0ba490df925d75a888d7
Erick Hitter <git-contrib@ethitter.com>
2022-08-29 17:29:20 -0700

    Remove debug line

--- scripts/set-wordpress-envars.sh
+++ scripts/set-wordpress-envars.sh
@@ -8,7 +8,6 @@ echo "WP_TESTS_DIR=${RUNNER_TEMP}/wp/tests-lib" >> $GITHUB_ENV
 echo "PMC_PLUGINS_DIR=${RUNNER_TEMP}/wp/core/wp-content/plugins/pmc-plugins" >> $GITHUB_ENV
 echo "PMC_PHPUNIT_BOOTSTRAP=${RUNNER_TEMP}/wp/core/wp-content/plugins/pmc-plugins/pmc-unit-test/bootstrap.php" >> $GITHUB_ENV
 
-echo "'${GITHUB_REF_TYPE}' '$(pmc_get_git_default_branch)' '${BITBUCKET_BRANCH}'"
 if [[ "branch" == "${GITHUB_REF_TYPE}" && "$(pmc_get_git_default_branch)" == "${BITBUCKET_BRANCH}" ]]; then
   echo "PMC_PHPUNIT_MATCH_BRANCH=${BITBUCKET_BRANCH}" >> $GITHUB_ENV
 else
