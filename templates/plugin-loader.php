0862cf05f07947472e28831ba217fc48c1fc6a4f
Erick Hitter <ehitter@pmc.com>
2022-11-03 16:00:36 -0700

    Debug failing plugin loader

--- templates/plugin-loader.php
+++ templates/plugin-loader.php
@@ -10,9 +10,10 @@
 
 // Cannot use `STYLESHEETPATH` as it isn't set yet.
 $theme_plugins_path = get_stylesheet_directory() . '/client-mu-plugins/';
-
+var_export( 'LOADING PLUGINS!', false );
 if ( wpcom_vip_should_load_plugins() && is_dir( $theme_plugins_path ) ) {
 	foreach ( wpcom_vip_get_client_mu_plugins( $theme_plugins_path ) as $client_mu_plugin ) {
+var_export( $client_mu_plugin, false );
 		include_once $client_mu_plugin;
 	}
 
