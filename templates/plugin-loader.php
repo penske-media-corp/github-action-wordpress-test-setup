4a2d0ea9f0804ad9ffcb5459a5e287be8c97132a
Erick Hitter <git-contrib@ethitter.com>
2024-10-16 12:05:41 -0700

    Account for parent theme's `client-mu-plugins` directory

--- templates/plugin-loader.php
+++ templates/plugin-loader.php
@@ -8,22 +8,42 @@
  * first place.
  */
 
-add_action( 
-	'muplugins_loaded', 
+add_action(
+	'muplugins_loaded',
 	static function(): void {
 		// Bail during the PHPUnit install step as it happens before the bootstrap sets necessary constants.
 		if ( ! class_exists( PMC\Unit_Test\Bootstrap::class, false ) ) {
 			return;
 		}
 
-		$theme_plugins_path = getenv( 'GITHUB_WORKSPACE' ) . '/client-mu-plugins/';
+		if ( ! wpcom_vip_should_load_plugins() ) {
+			return;
+		}
 
-		if ( wpcom_vip_should_load_plugins() && is_dir( $theme_plugins_path ) ) {
-			foreach ( wpcom_vip_get_client_mu_plugins( $theme_plugins_path ) as $client_mu_plugin ) {
-				include_once $client_mu_plugin;
+		$theme_plugins_paths = [];
+
+		if ( ! getenv( 'PMC_IS_PMC_PLUGINS' ) ) {
+			$test = sprintf(
+				'%1$s/wp-content/themes/%2$spmc-core-v2/client-mu-plugins/',
+				getenv( 'WP_CORE_DIR' ),
+				'true' === getenv( 'VIP_THEME' ) ? 'vip/' : '',
+			);
+var_export( $test );
+		}
+var_export( get_stylesheet_directory() );
+
+		$theme_plugins_paths[] = getenv( 'GITHUB_WORKSPACE' ) . '/client-mu-plugins/';
+
+		$theme_plugins_paths = array_unique( $theme_plugins_paths );
+var_export( $theme_plugins_paths );
+		foreach ( $theme_plugins_paths as $theme_plugins_path ) {
+			if ( is_dir( $theme_plugins_path ) ) {
+				foreach ( wpcom_vip_get_client_mu_plugins( $theme_plugins_path ) as $client_mu_plugin ) {
+					include_once $client_mu_plugin;
+				}
+
+				unset( $client_mu_plugin );
 			}
-
-			unset( $client_mu_plugin );
 		}
 	}
 );
